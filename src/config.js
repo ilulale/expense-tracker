c46f6da652192e539f9f0206150a1f8c19031270
ilulalel <kshitijtapre@gmail.com>
2022-02-16 12:53:01 +0530

    dockerisation

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 let config = {
-  // baseUrl: "http://192.168.1.13:3000",
-  baseUrl: "https://arcane-wave-10684.herokuapp.com",
+  baseUrl: "http://192.46.215.126:3000",
+  // baseUrl: "https://arcane-wave-10684.herokuapp.com",
 };
 export default config;

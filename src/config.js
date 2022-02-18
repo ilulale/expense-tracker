ec1409a9497f758730ab8a34254c159419d9c470
ilulalel <kshitijtapre@gmail.com>
2022-02-19 02:30:25 +0530

    expenses notes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let config = {
-  // baseUrl: "http://192.46.215.126:3000",
-  baseUrl: "http://localhost:3000",
+  baseUrl: "http://192.46.215.126:3000",
+  // baseUrl: "http://localhost:3000",
   // baseUrl: "https://arcane-wave-10684.herokuapp.com",
 };
 export default config;
